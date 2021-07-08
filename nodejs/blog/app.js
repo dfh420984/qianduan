8f584955d7c82a0a748a70bc9bbd3f92ec7f15f2
duanfuhao <duanfuhao@smzdm.com>
2021-07-08 18:47:32 +0800

    blog 初始化

--- nodejs/blog/app.js
+++ nodejs/blog/app.js
@@ -6,6 +6,9 @@ var app = express()
 app.use('/public/', express.static(path.join(__dirname, './public/')))
 app.use('/node_modules/', express.static(path.join(__dirname,'./node_modules/')))
 
+app.engine('html', require('express-art-template'))
+app.set('views', path.join(__dirname, './views/')) // 默认就是 ./views 目录
+
 app.get('/', function(req, res){
 	res.send('hello world')
 })

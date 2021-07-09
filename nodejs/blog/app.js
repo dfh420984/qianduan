fbdcfae352b282eaacf94ac12d508314f3fd6812
duanfuhao <duanfuhao@smzdm.com>
2021-07-09 11:45:01 +0800

    nodejs blog day6

--- nodejs/blog/app.js
+++ nodejs/blog/app.js
@@ -1,16 +1,19 @@
 var express = require('express')
 var path = require('path')
+var router = require('./router')
 
 var app = express()
-
-app.use('/public/', express.static(path.join(__dirname, './public/')))
-app.use('/node_modules/', express.static(path.join(__dirname,'./node_modules/')))
 
 app.engine('html', require('express-art-template'))
 app.set('views', path.join(__dirname, './views/')) // 默认就是 ./views 目录
 
+app.use('/public/', express.static(path.join(__dirname, './public/')))
+app.use('/node_modules/', express.static(path.join(__dirname,'./node_modules/')))
+//路由挂载到app中
+app.use(router)
+
 app.get('/', function(req, res){
-	res.send('hello world')
+	res.render('index.html')
 })
 
 app.listen(3000, function(){

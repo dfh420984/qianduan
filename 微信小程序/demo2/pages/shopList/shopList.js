7b2fca85110bc151834e55c41458bbdcaad3879b
duanfuhao <duanfuhao@smzdm.com>
2021-07-28 17:21:34 +0800

    小程序day2 end

--- 微信小程序/demo2/pages/shopList/shopList.js
+++ 微信小程序/demo2/pages/shopList/shopList.js
@@ -5,14 +5,26 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    shopList: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.request({
+      url: "https://locally.uieee.com/categories/" + options.cat + "/shops",
+      data: {
+          _limit: 20,
+          _page: 1
+      },
+      success: (res) => {
+          console.log(res);
+          this.setData({
+              shopList: res.data
+          })
+      },
+    })
   },
 
   /**

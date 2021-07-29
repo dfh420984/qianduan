876fbda7cc8481bc3930f0abbb0d644a1458b160
duanfuhao <duanfuhao@smzdm.com>
2021-07-29 16:11:53 +0800

    微信小程序day3

--- 微信小程序/demo2/pages/shopList/shopList.js
+++ 微信小程序/demo2/pages/shopList/shopList.js
@@ -6,8 +6,26 @@ Page({
    */
   data: {
     shopList: [],
+    pageIndex: 0,
+    pageSize: 20,
+    catId: 1
   },
-
+  loadMore: function() {
+    wx.request({
+      url: "https://locally.uieee.com/categories/" + this.data.catId + "/shops",
+      data: {
+          _limit: this.data.pageSize,
+          _page: ++this.data.pageIndex
+      },
+      success: (res) => {
+          console.log(res);
+          var newList = this.data.shopList.concat(res.data)
+          this.setData({
+              shopList: newList
+          })
+      },
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -15,19 +33,10 @@ Page({
     wx.setNavigationBarTitle({
       title: options.title || '美食列表',
     })
-    wx.request({
-      url: "https://locally.uieee.com/categories/" + options.cat + "/shops",
-      data: {
-          _limit: 20,
-          _page: 1
-      },
-      success: (res) => {
-          console.log(res);
-          this.setData({
-              shopList: res.data
-          })
-      },
+    this.setData({
+      catId: options.cat
     })
+    this.loadMore()
   },
 
   /**
@@ -62,14 +71,20 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    console.log("下拉事件")
+    this.setData({
+      shopList: [],
+      pageIndex: 0
+    })
+    this.loadMore()
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    console.log("上拉触底")
+    this.loadMore()
   },
 
   /**
